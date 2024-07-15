572967831125d07a8813e42034b91be31581047c
Vova Lando <vova.lando@gmail.com>
2024-07-15 19:49:44 +0300

    Missing data-source config

---
+++ src/config/data-source.config.ts
@@ -0,0 +1,38 @@
+import { DataSourceOptions } from 'typeorm';
+
+const getDataSourceConfig = (): DataSourceOptions => {
+  const defaultConfig = {
+    entities: [`${__dirname}/../entities/**/*{.ts,.js}`],
+    migrations: [`${__dirname}/../migrations/**/*{.ts,.js}`],
+  };
+
+  switch (process.env.NODE_ENV) {
+    case 'production':
+      return {
+        ...defaultConfig,
+        type: 'postgres',
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        port: Number(process.env.DATABASE_PORT),
+        url: process.env.DATABASE_URL,
+        synchronize: false,
+        ssl: { rejectUnauthorized: false },
+      };
+    case 'test':
+      return {
+        ...defaultConfig,
+        type: 'sqlite',
+        database: ':memory:',
+        synchronize: true,
+      };
+    default: // development
+      return {
+        ...defaultConfig,
+        type: 'sqlite',
+        database: 'kalatori.sqlite',
+        synchronize: false,
+      };
+  }
+};
+
+export default getDataSourceConfig;
